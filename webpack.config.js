8e90a0ac106b1d25ea330dd9d05ad5c4ad40017a
Akash Kumar <akash.sky.heaven@gmail.com>
2021-07-11 16:25:07 +0530

    added file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports ={
         rules:[
             {test: /\.(js)$/, use: 'babel-loader'},
             {test: /\.(css)$/, use: ['style-loader', 'css-loader']},
-            { test: /\.(png|jpg)$/, use: 'url-loader?limit=8192', }
+            { test: /\.(png|jpg)$/, use: 'url-loader?limit=8192', },
+            { test: /\.(jpe?g|gif|png|svg|woff|ttf|wav|mp3)$/, options: {name: '[path][name].[ext]'},loader: "file-loader" }
         ]
     },
     output: {
