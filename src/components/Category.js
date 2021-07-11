b0ca7c1f0519ad57f4ee11c0e4c48abdc3b93a42
Akash Kumar <akash.sky.heaven@gmail.com>
2021-07-11 16:26:58 +0530

    fixed image path not found issue

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 import '../styles.css';
 
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const images = importAll(require.context('../../public/img/categories', false, /\.(png|jpe?g|svg)$/));
+
 export default function Category (props){
     const {categories, selectedCategory} = props;
     return (
@@ -11,9 +19,8 @@ export default function Category (props){
             {categories.map((category)=>{
                 return (
                     <li className="category-item" key={category}>
-                        {/* <button className="category-btn" value={category} onClick={selectedCategory}>{category}</button> */}
                         <div className="card">
-                            <img src={`../src/img/categories/${category}.jpg`} alt={category} className="category-thumbnail" onClick={selectedCategory}></img>
+                            <img src={`./public/img/categories/${category}.jpg`} alt={category} className="category-thumbnail" onClick={selectedCategory}></img>
                             <div className="banner-title">
                                 <div className="label">{category}</div>
                             </div>
